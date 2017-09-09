47f583f1c47bd0f22fa947b0e6afd8a82cf71dc4
Shalini1998 <saloriya1998@gmail.com>
2017-09-09 15:33:43 +0530

    sign in

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -78,6 +78,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:title, :description, :status)
+      params.require(:task).permit(:title, :description, :status, :user_id)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create([
+  {email:"pandeynikita123@gmail.com" , password:"nsds123" , role:"developer"},
+  {email:"ankitadangi11@gmail.com" , password:"nsds123" , role:"developer"},
+  {email:"apoorvmishra2@gmail.com" , password:"nsds123" , role:"developer"},
+  {email:"ravi.singh1308@gmail.com" , password:"admin123" , role:"admin"},
+  {email:"barpit111@gmail.com" , password:"nsds123" , role:"developer"},
+  {email:"dheerendrapandey668@gmail.com" , password:"nsds123" , role:"developer"},
+  {email:"saloriya1997@gmail.com" , password:"admin123" , role:"admin"}
+])
+
+User.create(email:"pandeynikita123@gmail.com" , password:"nsds123" , role:"developer")
