e791da48eb4d48b1f8029ff58479cdfd1f7f5597
Shalini1998 <saloriya1998@gmail.com>
2017-09-09 12:04:49 +0530

    production assets true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Handles cancancan exceptions
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
+  end
  
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
-  end
 end
