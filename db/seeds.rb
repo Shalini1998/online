10a2f13692e3187100c02090ef1873a39b70a336
Shalini1998 <saloriya1998@gmail.com>
2017-09-13 17:45:46 +0530

    seed commented

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-users = User.create([{email:"pandeynikita123@gmail.com" , password:"nsds123" , role:"developer"},{email:"ankitadangi11@gmail.com" , password:"nsds123" , role:"developer"},{email:"apoorvmishra2@gmail.com" , password:"nsds123" , role:"developer"},{email:"ravi.singh1308@gmail.com" , password:"admin123" , role:"admin"}, {email:"barpit111@gmail.com" , password:"nsds123" , role:"developer"},{email:"dheerendrapandey668@gmail.com" , password:"nsds123" , role:"developer"},{email:"saloriya1997@gmail.com" , password:"admin123" , role:"admin"}])
+# users = User.create([{email:"pandeynikita123@gmail.com" , password:"nsds123" , role:"developer"},{email:"ankitadangi11@gmail.com" , password:"nsds123" , role:"developer"},{email:"apoorvmishra2@gmail.com" , password:"nsds123" , role:"developer"},{email:"ravi.singh1308@gmail.com" , password:"admin123" , role:"admin"}, {email:"barpit111@gmail.com" , password:"nsds123" , role:"developer"},{email:"dheerendrapandey668@gmail.com" , password:"nsds123" , role:"developer"},{email:"saloriya1997@gmail.com" , password:"admin123" , role:"admin"}])
