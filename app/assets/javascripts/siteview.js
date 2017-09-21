4b028398f3e33c8bbc8360967860ae906bd6c0e6
Shalini1998 <saloriya1998@gmail.com>
2017-09-21 10:28:50 +0530

    Removing JS

--- app/assets/javascripts/siteview.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require siteviews/bootstrap
-//= require siteviews/bootstrap-select
-//= require siteviews/jquery.slimscroll
-//= require siteviews/waves
-//= require siteviews/admin
-//= require siteviews/demo
